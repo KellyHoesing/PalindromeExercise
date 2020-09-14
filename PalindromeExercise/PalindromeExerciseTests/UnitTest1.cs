2d433f5cbf2df2d3c9f8db9a0400dd1ff922ca18
Kelly <kelly.hoesing@gmail.com>
2020-09-14 14:59:37 -0500

    Fixed up

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("phone", false)]
+        [InlineData("Racecar", true)]
+        public void PalindromeTester(string word, bool expected)
         {
-
+            var test = new WordSmith();
+            var actual = test.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
